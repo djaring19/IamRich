6a977d3141a4a3d00a84c1e2f763522dafbd362a
Don Riz Jaring <djaring@yahoo.com>
2021-10-06 17:47:36 +0800

    first commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Don Riz Jaring on 5/31/21.
+//  Copyright © 2021 Don Riz Jaring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
